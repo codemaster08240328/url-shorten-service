4d4ede2bd865d0b2e3523fcb7dd38a6a9c20dfad
Code Master <codemaster@Codes-MacBook-Pro.local>
2021-01-25 12:40:26 -0800

    Integration Test add feature file

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import express from 'express';
 
 const app: express.Application = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000');
 });
